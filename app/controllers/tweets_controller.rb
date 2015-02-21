2441866a95a0822286cad767fc41434d272ad6d9
Philip Greenwald <philip@avidest.com>
2015-02-21 14:35:43 -0500

    created new tweet form and create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,18 @@
 class TweetsController < ApplicationController
 	def new
+		@tweet = Tweet.new
+	end
+
+	def create
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+
+		redirect_to new_tweet_path
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'static_pages#main_page'
 
-   resources :tweets
+   resources 'tweets'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
